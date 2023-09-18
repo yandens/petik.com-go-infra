25da2880d90afb02f1d70343a89c1909528c50a2
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-09-18 09:18:42 +0800

    add crendentials.json, make bucket, and use the bucket as backend

---
+++ src/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "gcs" {
+    bucket  = "bucket-state-terraform-cicd"
+    prefix  = "terraform/state"
+    credentials = "../.secret/credentials.json"
+  }
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.64.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_storage_bucket" "terraform_cicd_state" {
+  name          = "bucket-state-terraform-cicd"
+  location      = var.region
+  force_destroy = false
+  public_access_prevention = "enforced"
+}
---
+++ src/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type        = string
+  description = "Project ID from Google Cloud Console"
+}
+
+variable "region" {
+  type        = string
+  description = "Region to Deploy the Infrastructure"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone to Deploy the Infrastructure"
+}
