94c2bb18ff8ea04fa7083532ab90c14712ca76f0
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-10-05 13:40:05 +0800

    add public access prevention to bucket

--- src/modules/storage/main.tf
+++ src/modules/storage/main.tf
@@ -4,6 +4,7 @@ resource "google_storage_bucket" "terraform-state" {
   storage_class               = "STANDARD"
   force_destroy               = false
   uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
   autoclass {
     enabled = true
   }
