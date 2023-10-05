17d7ca6c2d692b4cec13916a917e02b0581a9dce
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-10-05 13:48:26 +0800

    delete old bucket state

--- src/main.tf
+++ src/main.tf
@@ -19,13 +19,6 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_storage_bucket" "terraform_cicd_state" {
-  name          = "bucket-state-terraform-cicd"
-  location      = var.region
-  force_destroy = false
-  public_access_prevention = "enforced"
-}
-
 module "vpc" {
   source = "./modules/network"
   region = var.region
